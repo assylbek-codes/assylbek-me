2629fd421300571f96b263cd3d879a27d4f3d4e0
assylbek-saduakhassov-qlub <assylbek.saduakhassov@qlub.io>
2025-03-31 03:34:03 +0400

    fixes

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  // Indicated that the app should be deployed to a target other than "server"
+  output: "export",
+  // Required for static export of Next.js app
+  images: {
+    unoptimized: true,
+  },
+  // Ensure trailing slashes are used
+  trailingSlash: true,
 };
 
 export default nextConfig;
